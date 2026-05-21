03bcc954e312aa71d3321e8de70f45a0e44f6736
arno0392 <57816174+arno0392@users.noreply.github.com>
2026-05-21 09:21:02 +0200

    Update main.py

--- addon/rootfs/app/main.py
+++ addon/rootfs/app/main.py
@@ -45,7 +45,7 @@ logger = logging.getLogger(__name__)
 CONFIG_PATH = os.getenv("CONFIG_PATH", "/data")
 ENOCEAN_PORT = os.getenv("ENOCEAN_PORT", "")
 CACHE_DEVICE_STATES = os.getenv("CACHE_DEVICE_STATES", "true").lower() == "true"
-VERSION = "1.4.0"
+VERSION = "1.4.1"
 
 # Path to HA Supervisor options file
 OPTIONS_FILE = "/data/options.json"
